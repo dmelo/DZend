46cba40a2229fdb1390371bb27c5cd115e569959
Diogo Oliveira de Melo <dmelo87@gmail.com>
2012-01-18 20:35:51 -0200

    when login is required, $this->_loginRequired must be set to true before preDispatch

--- Controller/Action.php
+++ Controller/Action.php
@@ -4,6 +4,7 @@ class DZend_Controller_Action extends Zend_Controller_Action
 {
     protected $_session;
     protected $_request;
+    protected $_loginRequired;
 
     public function init()
     {
@@ -12,5 +13,14 @@ class DZend_Controller_Action extends Zend_Controller_Action
 
         $this->_session = DZend_Session_Namespace::get('session');
         $this->_request = $this->getRequest();
+
+    }
+
+    public function preDispatch()
+    {
+        if($this->_loginRequired && !isset($this->_session->user)) {
+            echo $this->view->t('Permission denied');
+            die;
+        }
     }
 }
