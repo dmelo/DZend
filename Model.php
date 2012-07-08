c70467d17ed1688c509f08f7177ffa0b37b7c4e2
Diogo Oliveira de Melo <dmelo87@gmail.com>
2012-07-08 14:45:11 -0300

    small fix

--- Model.php
+++ Model.php
@@ -15,12 +15,12 @@ class DZend_Model
     {
         // Attributs with preg matching ^_.*Model are automagically
         // initialized.
-        if (preg_match('/^_.*Model/', $name)) {
+        if (preg_match('/^_.*Model$/', $name)) {
             $className = ucfirst(preg_replace('/Model$/', '', preg_replace('/^_/', '', $name)));
             $this->$name = new $className();
 
             return $this->$name;
-        } else if (preg_match('/^_.*Db/', $name)) { // Attributs with
+        } else if (preg_match('/^_.*Db$/', $name)) { // Attributs with
             // preg matching ^_.*Db are automagically inizilized.
             $className = 'DbTable_' . ucfirst(preg_replace('/Db$/', '', preg_replace('/^_/', '', $name)));
             $this->$name = new $className();
