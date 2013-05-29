c00403676b0e3ccdb66f07cb95a2db40d308a30a
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-05-28 23:31:45 -0300

    documents trait CurrentUser

--- CurrentUser.php
+++ CurrentUser.php
@@ -1,9 +1,18 @@
 <?php
 
+/**
+ * Commonly used methods for users manipulated on session.
+ *
+ */
 trait DZend_CurrentUser
 {
     protected $_userRow = null;
 
+    /**
+     * _getUserRow Get current user database row
+     *
+     * @return void
+     */
     protected function _getUserRow()
     {
         if (null === $this->_userRow) {
@@ -15,6 +24,11 @@ trait DZend_CurrentUser
         return $this->_userRow;
     }
 
+    /**
+     * _getUserId Get current user id.
+     *
+     * @return void
+     */
     protected function _getUserId()
     {
         return $this->_getUserRow()->id;
