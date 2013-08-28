e022ac3b13bb4d478ac941c0765d4e310d81f274
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-08-28 06:29:17 -0300

    use the current request_scheme on domain

--- Application/Bootstrap/Bootstrap.php
+++ Application/Bootstrap/Bootstrap.php
@@ -29,7 +29,7 @@ class DZend_Application_Bootstrap_Bootstrap extends
     {
         $domain = null;
         if (array_key_exists('HTTP_HOST', $_SERVER)) {
-            $domain = 'http://' . $_SERVER['HTTP_HOST'];
+            $domain = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
             Zend_Registry::set('domain', $domain);
         } else {
             Zend_Registry::set('domain', '');
