27a0e06d025dc5aa9ee77e02444025f47cf878ea
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-09-25 04:50:13 -0300

    minor improvement on A helper

--- View/Helper/A.php
+++ View/Helper/A.php
@@ -2,8 +2,15 @@
 
 class DZend_View_Helper_A extends Zend_View_Helper_Abstract
 {
-    public function a($href, $innerHtml)
+    public function a($href, $innerHtml = null, array $attrs = array())
     {
-        return '<a href="' . $href . '">' . $innerHtml . '</a>';
+        null === $innerHtml && $innerHtml = $href;
+        $ret = '<a href="' . $href . '" ';
+        foreach ($attrs as $key => $value) {
+            $ret .= " $key=\"$value\"";
+        }
+        $ret .= '>' . $innerHtml . '</a>';
+
+        return $ret;
     }
 }
