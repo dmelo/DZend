b24f784908cc96ee66e22be552a183238ed50c70
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-10-06 17:37:57 -0300

    adjusting phpunit and instruction about how to run the tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,18 +2,13 @@
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../Application'));
+    || define(
+        'APPLICATION_PATH', realpath(dirname(__FILE__) . '/../Application')
+);
 
 // Define application environment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', 'testing');
-
-// Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../..'),
-    get_include_path(),
-)));
-
 
 
 require_once 'Zend/Application.php';
