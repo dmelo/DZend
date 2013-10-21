52e708eb0e8cfaf749d421bfd56848d10ac21f1a
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-10-20 23:24:36 -0200

    Use Facebook PHP SDK to authenticate users

--- Auth/Adapter/Facebook.php
+++ Auth/Adapter/Facebook.php
@@ -3,32 +3,49 @@
 class DZend_Auth_Adapter_Facebook implements Zend_Auth_Adapter_Interface
 {
     private $_identity;
+    private $_name;
 
     // TODO: implement request validation to check if the request was really
     // facebook's.
     public function authenticate()
     {
-        $facebook = new Facebook();
-        $facebook->setAppId(Zend_Registry::get('facebookId'));
-        $facebook->setAppSecret(Zend_Registry::get('facebookSecret'));
+        $facebook = new Facebook(
+            array(
+                'appId' => Zend_Registry::get('facebookId'),
+                'secret' => Zend_Registry::get('facebookSecret')
+            )
+        );
         $logger = Zend_Registry::get('logger');
         $logger->debug('facebook --------- ' . $facebook->getUser());
         try {
             $profile = $facebook->api('/me');
+            $this->setIdentity($profile['email']);
+            $this->setName($profile['name']);
             return new Zend_Auth_Result(
-                Zend_Auth_Result::SUCCESS, $profile['email']
+                Zend_Auth_Result::SUCCESS, $this->_identity
             );
         } catch (FacebookApiException $e) {
             $logger->err(
                 'Error athenticating user on facebook '
-                . $e->getMessage() . ' # ' . $e->getStackAsString()
+                . $e->getMessage() . ' # ' . $e->getTraceAsString()
             );
-            return new Zend_Auth_Result(Zend_Auth::FAILURE);
+            return new Zend_Auth_Result(Zend_Auth_Result::FAILURE, $this->_identity);
         }
     }
 
     public function setIdentity($identity)
     {
         $this->_identity = $identity;
+    }
+
+    public function setName($name)
+    {
+        $this->_name = $name;
+    }
+
+
+    public function getName()
+    {
+        return $this->_name;
     }
 }
