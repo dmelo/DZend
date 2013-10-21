883b419e39ca5b1e3de4529e07f730f3a867fa27
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-10-20 22:39:04 -0200

    better way to interact with facebook

--- Auth/Adapter/Facebook.php
+++ Auth/Adapter/Facebook.php
@@ -8,9 +8,23 @@ class DZend_Auth_Adapter_Facebook implements Zend_Auth_Adapter_Interface
     // facebook's.
     public function authenticate()
     {
-        return new Zend_Auth_Result(
-            Zend_Auth_Result::SUCCESS, $this->_identity
-        );
+        $facebook = new Facebook();
+        $facebook->setAppId(Zend_Registry::get('facebookId'));
+        $facebook->setAppSecret(Zend_Registry::get('facebookSecret'));
+        $logger = Zend_Registry::get('logger');
+        $logger->debug('facebook --------- ' . $facebook->getUser());
+        try {
+            $profile = $facebook->api('/me');
+            return new Zend_Auth_Result(
+                Zend_Auth_Result::SUCCESS, $profile['email']
+            );
+        } catch (FacebookApiException $e) {
+            $logger->err(
+                'Error athenticating user on facebook '
+                . $e->getMessage() . ' # ' . $e->getStackAsString()
+            );
+            return new Zend_Auth_Result(Zend_Auth::FAILURE);
+        }
     }
 
     public function setIdentity($identity)
