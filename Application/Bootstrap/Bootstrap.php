2f68880fe844e6270324afbc85bdf6824bcdca3f
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-11-07 16:00:24 -0200

    add config to registry, to be accessible application wide

--- Application/Bootstrap/Bootstrap.php
+++ Application/Bootstrap/Bootstrap.php
@@ -96,4 +96,14 @@ class DZend_Application_Bootstrap_Bootstrap extends
             'facebookSecret', $this->getOption('facebook')['secret']
         );
     }
+
+    public function _initConfig()
+    {
+        $config = new Zend_Config_Ini(
+            APPLICATION_PATH . "/configs/application.ini", APPLICATION_ENV
+        );
+        Zend_Registry::set('config', $config);
+    }
+
+
 }
