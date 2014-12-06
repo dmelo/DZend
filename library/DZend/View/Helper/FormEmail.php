7213238c089ab14003bc22536b6bcfde0a5b966d
Diogo Oliveira de Melo <dmelo87@gmail.com>
2014-12-06 09:51:30 -0200

    add class form-control to input[type=email] html elements

--- library/DZend/View/Helper/FormEmail.php
+++ library/DZend/View/Helper/FormEmail.php
@@ -5,6 +5,13 @@ class DZend_View_Helper_FormEmail extends Zend_View_Helper_FormText
     public function formEmail($name, $value = null, $attribs = null)
     {
         $xhtml = $this->formText($name, $value, $attribs);
-        return str_replace("type=\"text\"", "type=\"email\"", $xhtml);
+        if (strpos('class="', $xhtml) !== false) {
+            $xhtml = str_replace('class="', 'class="form-control ', $xhtml);
+            $xhtml = str_replace("type=\"text\"", "type=\"email\"", $xhtml);
+        } else {
+            $xhtml = str_replace("type=\"text\"", "type=\"email\" class=\"form-control\"", $xhtml);
+        }
+
+        return $xhtml;
     }
 }
